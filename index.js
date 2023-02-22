949a94aa979770732228c06bdbb67595a65a1b1f
crushwan <104525996+crushwan@users.noreply.github.com>
2023-02-22 00:56:54 +0000

    "submit"

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/'})
 
 var app = express();
 
@@ -18,3 +20,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }) 
+})
